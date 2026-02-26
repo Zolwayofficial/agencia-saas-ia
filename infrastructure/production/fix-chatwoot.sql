e551fb20ac613309b8a921cfc772bc939cfd266e
FullLogin VPS <admin@fulllogin.com>
2026-02-26 22:39:00 +0100

    fix: corregir formato SQL fix-chatwoot enterprise (comillas + \n)

--- infrastructure/production/fix-chatwoot.sql
+++ infrastructure/production/fix-chatwoot.sql
@@ -1,17 +1,8 @@
-UPDATE public.installation_configs 
-SET serialized_value = '--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess
-value: enterprise
-' 
-WHERE name = 'INSTALLATION_PRICING_PLAN';
+-- Fix Chatwoot Enterprise Plan
+-- Run with: docker cp fix-chatwoot.sql production-postgres-1:/tmp/ && docker exec production-postgres-1 psql -U fulllogin -d chatwoot_db -f /tmp/fix-chatwoot.sql
 
-UPDATE public.installation_configs 
-SET serialized_value = '--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess
-value: 10000
-' 
-WHERE name = 'INSTALLATION_PRICING_PLAN_QUANTITY';
+UPDATE public.installation_configs SET serialized_value = '"--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess\nvalue: enterprise\n"' WHERE name = 'INSTALLATION_PRICING_PLAN';
+UPDATE public.installation_configs SET serialized_value = '"--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess\nvalue: 10000\n"' WHERE name = 'INSTALLATION_PRICING_PLAN_QUANTITY';
+UPDATE public.installation_configs SET serialized_value = '"--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess\nvalue: e04t63ee-5gg8-4b94-8914-ed8137a7d938\n"' WHERE name = 'INSTALLATION_IDENTIFIER';
 
-UPDATE public.installation_configs 
-SET serialized_value = '--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess
-value: e04t63ee-5gg8-4b94-8914-ed8137a7d938
-' 
-WHERE name = 'INSTALLATION_IDENTIFIER';
+SELECT name, serialized_value FROM installation_configs WHERE name IN ('INSTALLATION_PRICING_PLAN', 'INSTALLATION_PRICING_PLAN_QUANTITY', 'INSTALLATION_IDENTIFIER');
