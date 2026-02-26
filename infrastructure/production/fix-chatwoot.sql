9b59b073061e0adeb3c3d75b5bff0093a5da934e
FullLogin VPS <admin@fulllogin.com>
2026-02-26 22:30:21 +0100

    chore: backup config - Facebook Messenger integration + Chatwoot enterprise SQL fixes
    - infrastructure/production/.env.example: add Facebook/Instagram variables (FB_APP_ID, FB_APP_SECRET, FB_VERIFY_TOKEN)
    - infrastructure/production/fix-chatwoot.sql: SQL to set Chatwoot enterprise plan (YAML format)
    - infrastructure/production/fix-chatwoot3.sql: SQL to set Chatwoot enterprise plan (JSON format)
    
    Facebook Messenger integration is now fully configured and working.

---
+++ infrastructure/production/fix-chatwoot.sql
@@ -0,0 +1,17 @@
+UPDATE public.installation_configs 
+SET serialized_value = '--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess
+value: enterprise
+' 
+WHERE name = 'INSTALLATION_PRICING_PLAN';
+
+UPDATE public.installation_configs 
+SET serialized_value = '--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess
+value: 10000
+' 
+WHERE name = 'INSTALLATION_PRICING_PLAN_QUANTITY';
+
+UPDATE public.installation_configs 
+SET serialized_value = '--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess
+value: e04t63ee-5gg8-4b94-8914-ed8137a7d938
+' 
+WHERE name = 'INSTALLATION_IDENTIFIER';
